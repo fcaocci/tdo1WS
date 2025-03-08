ac41288299da67e11d79bff59405deb59284fa42
fcaocci <fcaocci@gmail.com>
2025-03-08 20:17:45 +0100

    first commit

---
+++ src/main/java/com/todoWS/springboot/todoWS/TodoWsApplication.java
@@ -0,0 +1,13 @@
+package com.todoWS.springboot.todoWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todoWS/springboot/todoWS/TodoWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todoWS.springboot.todoWS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
